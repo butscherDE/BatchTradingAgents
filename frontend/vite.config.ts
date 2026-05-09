b550dff3f67bdb545b2884774c89ba4cb26b3062
ButschDE <sophie@metzger.pro>
2026-05-09 10:24:59 -0700

    updated vite config to host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5175,
     proxy: {
       '/api': 'http://10.0.0.217:8000',
