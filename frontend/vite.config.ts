2e205b912004833a462025612943c899af6a7139
ButschDE <sophie@metzger.pro>
2026-05-10 08:36:16 -0700

    allowlist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5175,
-    allowedHosts: true,
+    allowedHosts: ["home.metzger.pro"],
     proxy: {
       '/api': 'http://10.0.0.217:8000',
       '/ws': {
